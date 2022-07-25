bd0ceff357dc3cecd37f0a8a2d58c5784cc7de71
sheinix <jnuvreni@gmail.com>
2022-07-25 13:50:44 +1200

    remove unnecesary code

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -22,32 +22,6 @@ async function main() {
     heroContract.address,
     process.env.CHAINLINK_MUMBAI_SUBSCRIPTION_ID
     );
-  
-  // await heroNFTContract.deployed();
-
-  // console.log("Minter: " + minter)
-  // console.log("Sending money to minter: " + minter)
-
-  // let nftPrice = hre.ethers.utils.parseEther("15")
-  // let minterBalance = hre.ethers.utils.parseEther("150")
-
-  // let transactionReceipt = await heroContract.transfer(minter, minterBalance)
-  
-  // await transactionReceipt.wait(1);
-
-  // const balanceOfMinter = await heroContract.balanceOf(minter);
-
-  // console.log("Minter Balance: " + balanceOfMinter)
-  
-  // await heroContract.approve(heroNFTContract.address, minterBalance)
-
-  // await heroNFTContract.safeMint(minter);
-
-
-  // let currentTokenCount = await heroNFTContract.getTokenIdCounter()
-
-  // console.log("CurrentTokenID Count:" + currentTokenCount)
-
 }
 
 // We recommend this pattern to be able to use async/await everywhere
